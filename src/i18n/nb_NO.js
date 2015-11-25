8a6bf398c4256fd985a8f8534f8c779da02ebcf2
Remi Aleksander Solås <remi@npolar.no>
2015-11-25 18:22:39 +0100

    Added en-US, ja-JP, nb-NO and nn-NO locales

---
+++ src/i18n/nb_NO.js
@@ -0,0 +1,29 @@
+(function() {
+	"use strict";
+
+	FileFunnel.i18n.nb_NO = {
+		// Button labels
+		cancel:     "Avbryt",
+		reset:      "Tilbakestill",
+		upload:     "Last opp",
+
+		// Size indicators
+		bytes:      " bytes",
+		gibiBytes:  " GiB",
+		kibiBytes:  " KiB",
+		mebiBytes:  " MiB",
+
+		// Placeholder texts
+		fileName:   "Filnavn",
+		fileSize:   "Filstørrelse",
+		fileType:   "Filtype",
+
+		// Status indicators
+		forbidden:  "Ikke autorisert for opplasting",
+		oversized:  "Filen er for stor for opplasting",
+		refused:    "Tilkobling nektet",
+		success:    "Opplasting gjennomført"
+	};
+
+	FileFunnel.i18n.no = FileFunnel.i18n.nb = FileFunnel.i18n.nb_NO;
+})();
