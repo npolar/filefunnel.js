84a5aad813cccd0b89ea58ba4b5651d31fb535e6
Remi Aleksander Solås <remi@npolar.no>
2015-12-01 17:29:59 +0100

    Moved DOM building to prototype function

--- src/filefunnel.js
+++ src/filefunnel.js
@@ -250,12 +250,27 @@
 			}
 
 			return (this.html = value);
+		},
+		get visible() {
+			return !this.dom.hasAttribute("hidden");
+		},
+		set visible(visible) {
+			if(!!visible) {
+				this.dom.removeAttribute("hidden");
+			} else {
+				this.dom.setAttribute("hidden", "");
+			}
+
+			return this.visible;
 		}
 	};
 
 	function FileFunnel(selector, options) {
 		this._files   = [];
+		this._form    = null;
 		this._i18n    = FileFunnel.i18n.en_GB;
+		this._parent  = Element(selector);
+		this._status  = FileFunnel.status.READY;
 
 		// Parse and merge options with defaults
 		this._options = parseOptions(options, {
@@ -263,214 +278,242 @@
 			chunked:    false,
 			chunkSize:  0x100000,       // Chunk byte size (1 MiB by default)
 			className:  "filefunnel",   // Dot-separated CSS class names
-			multiple:   false
+			multiple:   false           // Enable upload of multiple files
 		});
 
 		// Use locale specified in constructor options if specified, otherwise browser locale. Default locale as fallback
 		this.locale = (options.locale || (navigator ? (navigator.userLanguage || navigator.language).replace("-", "_") : null));
-
-		var i18n = this._i18n, options = this._options, files = this._files;
-		var acceptTypes = ("string" == typeof options.accept ? options.accept : "*/*");
-		var multiAttrib = (true === options.multiple ? "[multiple]" : "");
-
-		// Create DOM form and child elements
-		var elems = this.elements = {
-			form:           new Element("form[enctype=multipart/form-data]." + options.className, { appendTo: Element(selector) }),
-			browseButton:   new Element("input[type=button][value=" + (options.multiple ? i18n.browseMultiple : i18n.browse) + "].browse"),
-			fileInput:      new Element("input[type=file][accept=" + acceptTypes + "][hidden]" + multiAttrib),
-			fileList:       new Element("div.filelist"),
-			submitButton:   new Element("input[type=submit][value=" + i18n.upload + "][disabled].submit"),
-			resetButton:    new Element("input[type=reset][value=" + i18n.reset + "].reset")
-		};
-
-		// Add child elements to form
-		elems.form.add([ elems.browseButton, elems.fileInput, elems.fileList, elems.submitButton, elems.resetButton ]);
-
-		// Enable file browsing using the browseButton proxy
-		elems.browseButton.on("click", function() {
-			elems.fileInput.dom.click();
-		});
-
-		// Handle chosen files
-		elems.fileInput.on("change", function(event) {
-			files.length = 0;
-			elems.fileList.clear();
-			elems.fileListItems = [];
-
-			[].slice.call(event.target.files).forEach(function(file) {
-				var fileSize = file.size, fileModified = file.lastModifiedDate, fileItemElems = {};
-
-				// Round filesize and add correct suffix for gibis, mibis and kibis
-				fileSize = (fileSize >= 0x40000000 ? ((fileSize / 0x40000000).toFixed(2) + i18n.gibiBytes) :
-					(fileSize >= 0x100000 ? ((fileSize / 0x100000).toFixed(2) + i18n.mebiBytes) :
-						(fileSize >= 0x400 ? ((fileSize / 0x400).toFixed(2) + i18n.kibiBytes) :
-							(fileSize += i18n.bytes))));
-
-				// Populate filelist DOM with new fileinfo item
-				elems.fileList.add(new Element("fieldset.fileinfo")
-					.append(new Element("legend", { html: file.name }))
-					.append((fileItemElems.name = new Element("input[type=text][placeholder=" + i18n.fileName + "][value=" + file.name + "].name")))
-					.append((fileItemElems.size = new Element("input[type=text][placeholder=" + i18n.fileSize + "][value=" + fileSize + "][disabled].size")))
-					.append((fileItemElems.type = new Element("input[type=text][placeholder=" + i18n.fileType + "][value=" + file.type + "][disabled].type")))
-					.append((fileItemElems.prog = new Element("progress[value=0].progress")))
-					.append((fileItemElems.info = new Element("span.info")))
-				);
-
-				files.push({ elements: fileItemElems, finished: false, reference: file });
-			});
-
-			// Enable/Disable the upload button
-			elems.submitButton.enabled = event.target.files.length;
-		});
-
-		// Handle form submit
-		elems.form.on("submit", function(event) {
-			event.preventDefault();
-
-			elems.fileInput.enabled = elems.submitButton.enabled = false;
-			elems.resetButton.value = i18n.cancel;
-
-			function finalizeUpload(file) {
-				file.finished = true;
-
-				for(var f in files) {
-					if(!files[f].finished) {
-						return;
-					}
-				}
-
-				elems.fileInput.enabled = true;
-				elems.resetButton.value = i18n.reset;
-			}
-
-			if(options.chunked) {
-				var chunkSize = ("number" == typeof options.chunkSize ? options.chunkSize: 0x100000);
-
-				files.forEach(function(file) {
-					var bytesSent = 0, bytesTotal = file.reference.size;
-					var fileName = file.elements.name.value;
-
-					var sendNextChunk = function() {
-						var chunk = file.reference.slice(bytesSent, Math.min(bytesTotal, bytesSent + chunkSize) + 1, file.reference.type);
-						var xhr = new XMLHttpRequest();
-
-						xhr.upload.onloadstart = function(e) {
-							file.elements.prog.attrib("max", bytesTotal + (e.lengthComputable ? e.total : 0));
-							file.elements.prog.value = bytesSent;
-						};
-
-						xhr.upload.onloadend = function() {
-							file.elements.prog.attrib("max", bytesTotal);
-							file.elements.prog.value = (bytesSent += chunk.size);
-
-							if(bytesSent < bytesTotal) {
-								if("function" == typeof sendNextChunk) {
-									sendNextChunk();
-								} else {
-									finalizeUpload(file);
-								}
-							} else {
-								file.elements.info.classes.add("success");
-								file.elements.info.value = i18n.success;
-								finalizeUpload(file);
-							}
-						};
-
-						xhr.upload.onprogress = function(e) {
-							(e.lengthComputable && (file.elements.prog.value = bytesSent + e.loaded));
-						};
-
-						xhr.onreadystatechange = function(e) {
-							if(e.target.status >= 400) {
-								file.elements.prog.attrib("max", (file.elements.prog.value = 0));
-								file.elements.info.classes.add("error");
-								sendNextChunk = null;
-								finalizeUpload(file);
-
-								// Decide error message from status code (fallback to status text)
-								switch(e.target.status) {
-									case 401: case 403:
-										file.elements.info.value = i18n.forbidden;
-										break;
-
-									case 413:
-										file.elements.info.value = i18n.oversized;
-										break;
-
-									default:
-										file.elements.info.value = e.target.statusText;
-								}
-							}
-						};
-
-						xhr.onerror = function() {
-							file.elements.prog.attrib("max", (file.elements.prog.value = 0));
-							file.elements.info.classes.add("error");
-							file.elements.info.value = i18n.refused;
-							sendNextChunk = null;
-							finalizeUpload(file);
-						};
-
-						xhr.open("POST", options.server, true);
-
-						xhr.setRequestHeader("Content-Type", chunk.type);
-						xhr.setRequestHeader("X-File-Name", fileName);
-						xhr.setRequestHeader("X-File-Size", bytesTotal);
-
-						xhr.send(chunk);
-					};
-
-					file.elements.prog.attrib("max", bytesTotal);
-					sendNextChunk();
-				});
-			} else {
-				var xhr = new XMLHttpRequest(), formData = new FormData();
-
-				xhr.onreadystatechange = function(e) {
-					if(e.target.status >= 200 && e.target.status < 300) {
-						files.forEach(function(file) {
-							file.elements.prog.attrib("max", e.total || 1);
-							file.elements.prog.attrib("value", e.loaded || 1);
-							file.elements.info.classes.add("success");
-							file.elements.info.value = i18n.success;
-							finalizeUpload(file);
-						});
-					}
-				};
-
-				xhr.onerror = function(e) {
-					files.forEach(function(file) {
-						file.elements.prog.attrib("max", (file.elements.prog.value = 0));
-						file.elements.info.classes.add("error");
-						file.elements.info.value = i18n.refused;
-						finalizeUpload(file);
-					});
-				};
-
-				files.forEach(function(file) {
-					formData.append(file.reference.name, file.reference, file.elements.name.value);
-					file.elements.prog.dom.removeAttribute("value");
-				});
-
-				xhr.open("POST", options.server, true);
-				xhr.send(formData);
-			}
-		});
-
-		// Handle form reset
-		elems.form.on("reset", function() {
-			elems.fileList.clear();
-			elems.fileListItems = [];
-			elems.fileInput.enabled = true;
-			elems.submitButton.enabled = false;
-			elems.resetButton.value = i18n.reset;
-		});
+		this.build();
 	}
 
-	FileFunnel.VERSION = 0.14;
+	FileFunnel.VERSION = 0.20;
+
+	FileFunnel.status = { READY: 0, UPLOADING: 1, COMPLETED: 3, ABORTED: 4, ERROR: 5 };
 
 	// Prototype methods
 	FileFunnel.prototype = {
+		build: function() {
+			var files = this._files, i18n = this._i18n, options = this._options, parent = this._parent;
+			var acceptTypes = ("string" == typeof options.accept ? options.accept : "*/*");
+			var multiAttrib = (true === options.multiple ? "[multiple]" : "");
+
+			// Create DOM form and child elements
+			var elems = {
+				form:           new Element("form[enctype=multipart/form-data]." + options.className),
+				browseButton:   new Element("input[type=button][value=" + (options.multiple ? i18n.browseMultiple : i18n.browse) + "].browse"),
+				fileInput:      new Element("input[type=file][accept=" + acceptTypes + "][hidden][multiple]"),
+				fileList:       new Element("div.filelist"),
+				submitButton:   new Element("input[type=submit][value=" + i18n.upload + "][disabled].submit"),
+				resetButton:    new Element("input[type=reset][value=" + i18n.reset + "].reset")
+			};
+
+			// Disable multi file upload if not explicitly defined in options
+			(true !== options.multiple && elems.fileInput.dom.removeAttribute("multiple"));
+
+			// Add child elements to form
+			elems.form.add([ elems.browseButton, elems.fileInput, elems.fileList, elems.submitButton, elems.resetButton ]);
+
+			// Add form element to DOM
+			if(this._form && this._form.parent) {
+				form.parent.replace((this._form = elems.form), this._form);
+			} else if(parent && parent.dom instanceof HTMLInputElement) {
+				elems.form.visible = false;
+				parent.parent.append((this._form = elems.form), parent);
+				parent.on("click", function() { this.show(); }, this);
+			} else if(parent) {
+				parent.append((this._form = elems.form));
+			}
+
+			// Enable file browsing using the browseButton proxy
+			elems.browseButton.on("click", function() {
+				elems.fileInput.dom.click();
+			});
+
+			// Handle chosen files
+			elems.fileInput.on("change", function(event) {
+				files.length = 0;
+				elems.fileList.clear();
+				elems.fileListItems = [];
+
+				[].slice.call(event.target.files).forEach(function(file) {
+					var fileSize = file.size, fileModified = file.lastModifiedDate, fileItemElems = {};
+
+					// Round filesize and add correct suffix for gibis, mibis and kibis
+					fileSize = (fileSize >= 0x40000000 ? ((fileSize / 0x40000000).toFixed(2) + i18n.gibiBytes) :
+						(fileSize >= 0x100000 ? ((fileSize / 0x100000).toFixed(2) + i18n.mebiBytes) :
+							(fileSize >= 0x400 ? ((fileSize / 0x400).toFixed(2) + i18n.kibiBytes) :
+								(fileSize += i18n.bytes))));
+
+					// Populate filelist DOM with new fileinfo item
+					elems.fileList.add(new Element("fieldset.fileinfo")
+						.append(new Element("legend", { html: file.name }))
+						.append((fileItemElems.name = new Element("input[type=text][placeholder=" + i18n.fileName + "][value=" + file.name + "].name")))
+						.append((fileItemElems.size = new Element("input[type=text][placeholder=" + i18n.fileSize + "][value=" + fileSize + "][disabled].size")))
+						.append((fileItemElems.type = new Element("input[type=text][placeholder=" + i18n.fileType + "][value=" + file.type + "][disabled].type")))
+						.append((fileItemElems.prog = new Element("progress[value=0].progress")))
+						.append((fileItemElems.info = new Element("span.info")))
+					);
+
+					files.push({ elements: fileItemElems, finished: false, reference: file, xhr: null });
+				});
+
+				// Enable/Disable the upload button
+				elems.submitButton.enabled = event.target.files.length;
+			});
+
+			// Handle form submit
+			elems.form.on("submit", function(event) {
+				var self = this;
+				event.preventDefault();
+
+				elems.fileInput.enabled = elems.submitButton.enabled = false;
+				elems.resetButton.value = i18n.cancel;
+				self._status = FileFunnel.status.UPLOADING;
+
+				function finalizeUpload(file) {
+					file.finished = true;
+
+					for(var f in files) {
+						if(!files[f].finished) {
+							return;
+						}
+					}
+
+					elems.fileInput.enabled = true;
+					elems.resetButton.value = i18n.reset;
+					self._status = FileFunnel.status.COMPLETED;
+				}
+
+				if(options.chunked) {
+					var chunkSize = ("number" == typeof options.chunkSize ? options.chunkSize: 0x100000);
+
+					files.forEach(function(file) {
+						var bytesSent = 0, bytesTotal = file.reference.size;
+						var fileName = file.elements.name.value;
+
+						var sendNextChunk = function() {
+							var chunk = file.reference.slice(bytesSent, Math.min(bytesTotal, bytesSent + chunkSize) + 1, file.reference.type);
+							var xhr = file.xhr = new XMLHttpRequest();
+
+							xhr.upload.onloadstart = function(e) {
+								file.elements.prog.attrib("max", bytesTotal + (e.lengthComputable ? e.total : 0));
+								file.elements.prog.value = bytesSent;
+							};
+
+							xhr.upload.onloadend = function() {
+								file.elements.prog.attrib("max", bytesTotal);
+								file.elements.prog.value = (bytesSent += chunk.size);
+
+								if(bytesSent < bytesTotal) {
+									if("function" == typeof sendNextChunk) {
+										sendNextChunk();
+									} else {
+										finalizeUpload(file);
+									}
+								} else {
+									file.elements.info.classes.add("success");
+									file.elements.info.value = i18n.success;
+									finalizeUpload(file);
+								}
+							};
+
+							xhr.upload.onprogress = function(e) {
+								(e.lengthComputable && (file.elements.prog.value = bytesSent + e.loaded));
+							};
+
+							xhr.onreadystatechange = function(e) {
+								if(e.target.status >= 400) {
+									self._status = FileFunnel.status.ERROR;
+									file.elements.prog.attrib("max", (file.elements.prog.value = 0));
+									file.elements.info.classes.add("error");
+									sendNextChunk = null;
+									finalizeUpload(file);
+
+									// Decide error message from status code (fallback to status text)
+									switch(e.target.status) {
+										case 401: case 403:
+											file.elements.info.value = i18n.forbidden;
+											break;
+
+										case 413:
+											file.elements.info.value = i18n.oversized;
+											break;
+
+										default:
+											file.elements.info.value = e.target.statusText;
+									}
+								}
+							};
+
+							xhr.onerror = function() {
+								self._status = FileFunnel.status.ERROR;
+								file.elements.prog.attrib("max", (file.elements.prog.value = 0));
+								file.elements.info.classes.add("error");
+								file.elements.info.value = i18n.refused;
+								sendNextChunk = null;
+								finalizeUpload(file);
+							};
+
+							xhr.open("POST", options.server, true);
+
+							xhr.setRequestHeader("Content-Type", chunk.type);
+							xhr.setRequestHeader("X-File-Name", fileName);
+							xhr.setRequestHeader("X-File-Size", bytesTotal);
+
+							xhr.send(chunk);
+						};
+
+						file.elements.prog.attrib("max", bytesTotal);
+						sendNextChunk();
+					});
+				} else {
+					var xhr = files.xhr = new XMLHttpRequest(), formData = new FormData();
+
+					xhr.onreadystatechange = function(e) {
+						if(e.target.status >= 200 && e.target.status < 300) {
+							files.forEach(function(file) {
+								self._status = FileFunnel.status.COMPLETED;
+								file.elements.prog.attrib("max", e.total || 1);
+								file.elements.prog.attrib("value", e.loaded || 1);
+								file.elements.info.classes.add("success");
+								file.elements.info.value = i18n.success;
+								finalizeUpload(file);
+							});
+						}
+					};
+
+					xhr.onerror = function(e) {
+						files.forEach(function(file) {
+							self._status = FileFunnel.status.ERROR;
+							file.elements.prog.attrib("max", (file.elements.prog.value = 0));
+							file.elements.info.classes.add("error");
+							file.elements.info.value = i18n.refused;
+							finalizeUpload(file);
+						});
+					};
+
+					files.forEach(function(file) {
+						formData.append(file.reference.name, file.reference, file.elements.name.value);
+						file.elements.prog.dom.removeAttribute("value");
+					});
+
+					xhr.open("POST", options.server, true);
+					xhr.send(formData);
+				}
+			});
+
+			// Handle form reset
+			elems.form.on("reset", function() {
+				elems.fileList.clear();
+				elems.fileListItems = [];
+				elems.fileInput.enabled = true;
+				elems.submitButton.enabled = false;
+				elems.resetButton.value = i18n.reset;
+			});
+		},
+		hide: function() {
+			(this._form && (this._form.visible = false));
+		},
 		get locale() {
 			return this._i18n;
 		},
@@ -479,6 +522,9 @@
 				this._i18n = FileFunnel.i18n[value];
 				// TODO: Locale hotswap
 			}
+		},
+		show: function() {
+			(this._form && (this._form.visible = true));
 		}
 	};
 
@@ -489,6 +535,7 @@
 			browse:         "Choose file",
 			browseMultiple: "Choose files",
 			cancel:         "Cancel",
+			delete:         "Delete",
 			reset:          "Reset",
 			upload:         "Upload",
 
