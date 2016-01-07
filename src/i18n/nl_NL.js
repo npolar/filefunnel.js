3a519da2e181e546fccaf36995fc9473d8d05a5e
RDuX <ruben.dens@gmail.com>
2016-01-07 15:08:11 +0100

    dutch translations

---
+++ src/i18n/nl_NL.js
@@ -0,0 +1,31 @@
+(function() {
+	"use strict";
+
+	FileFunnel.i18n.nl_NL = {
+		// Button labels
+		browse:         "Selecteer bestand",
+		browseMultiple: "Selecteer bestanden",
+		cancel:         "Annuleer",
+		reset:          "Wissen",
+		upload:         "Upload",
+
+		// Size indicators
+		bytes:          " bytes",
+		gibiBytes:      " GiB",
+		kibiBytes:      " KiB",
+		mebiBytes:      " MiB",
+
+		// Placeholder texts
+		fileName:       "Bestandsnaam",
+		fileSize:       "Bestandsgrootte",
+		fileType:       "Bestandsformaat",
+
+		// Status indicators
+		forbidden:      "Upload niet toegestaan",
+		oversized:      "Bestand is te groot",
+		refused:        "Verbinding geweigerd",
+		success:        "Bestand succesvol geupload"
+	};
+
+	FileFunnel.i18n.nl = FileFunnel.i18n.nl_NL;
+})();
