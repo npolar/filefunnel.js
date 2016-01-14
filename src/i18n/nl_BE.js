b642c9771bde9a5c5acf1b23891f29a2ac4587f9
Remi Aleksander Solås <remi@npolar.no>
2016-01-14 12:07:01 +0100

    Made nl_BE locale independent of nl_NL

--- src/i18n/nl_BE.js
+++ src/i18n/nl_BE.js
@@ -1,5 +1,33 @@
 (function() {
 	"use strict";
 
-	FileFunnel.i18n.nl_BE = FileFunnel.i18n.nl_NL;
+	FileFunnel.i18n.nl_BE = {
+		// Button labels
+		add:            "Voeg bestanden toe",
+		browse:         "Selecteer bestand",
+		cancel:         "Annuleer",
+		delete:         "Verwijderen",
+		reset:          "Wissen",
+		upload:         "Uploaden",
+
+		// Size indicators
+		bytes:          " bytes",
+		gibiBytes:      " GiB",
+		kibiBytes:      " KiB",
+		mebiBytes:      " MiB",
+
+		// Placeholder texts
+		fileName:       "Bestandsnaam",
+		fileSize:       "Bestandsgrootte",
+		fileType:       "Bestandsformaat",
+
+		// Status indicators
+		aborted:        "Upload geannuleerd",
+		failed:         "Upload mislukt",
+		forbidden:      "Upload niet toegestaan",
+		oversized:      "Bestand is te groot",
+		refused:        "Verbinding geweigerd",
+		success:        "Bestand succesvol geupload",
+		timeout:        "Tijd verstreken"
+	};
 })();
